787a43891277642af9e0a79f24316bd97d17a9cd
Akinnusoye Taiwo <taiwoakinnusoye@gmail.com>
2019-11-05 12:02:04 +0100

    item

--- js/kanban.js
+++ js/kanban.js
@@ -24,6 +24,13 @@ const create_item = () => {
   item.addEventListener('dragstart', (event) => {
     return event.dataTransfer.setData('text', event.target.id)
   });
+
+  item.addEventListener('dragend', (event) => {
+    event.dataTransfer.clearData()
+  });
+
+  const input = document.createElement('input');
+  item.append(input);
 };
 
 document.querySelectorAll('.drop').forEach(element => {
